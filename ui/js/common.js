0e7f3ca9c4c410e63f4b38408ff9955f45054f9b
sosim13 <sosim13p@gmail.com>
2018-04-27 10:35:07 +0900

    321

---
+++ ui/js/common.js
@@ -0,0 +1,29 @@
+﻿function hideElement(id)
+{
+	var element = document.getElementById(id);
+
+	if (element)
+  		element.style.display='none';
+}
+
+function hideElementByClass(className)
+{
+  	var tags=document.getElementsByClassName(className);
+  	
+	for (var i = 0; i < tags.length; ++i) {
+  		var element = tags[i];
+  		element.style.display='none';
+  	}		
+}
+
+function css(selector, property, value) {
+    for (var i=0; i < document.styleSheets.length; ++i) {
+        try { 
+        	document.styleSheets[i].insertRule(selector+ ' {'+property+':'+value+'}', document.styleSheets[i].cssRules.length);
+        } catch(err) {
+        }
+    }
+}
+
+css("body", "font-size", "30px");
+css("a:link", "font-size", "30px");
